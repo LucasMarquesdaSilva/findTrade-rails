495e0f51636605c4922077910543760abb59f2f1
LucasMarquesdaSilva <lucas.msilva09@gmail.com>
2016-12-16 17:53:52 -0200

    Adding authentication with devise

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,8 +24,9 @@ class ProductsController < ApplicationController
   # POST /products
   # POST /products.json
   def create
-    @product = Product.new(product_params)
 
+    @product = Product.new(product_params)
+    @product.user = current_user
     respond_to do |format|
       if @product.save
         format.html { redirect_to @product, notice: 'Product was successfully created.' }
@@ -69,6 +70,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :description, :price, :category)
+      params.require(:product).permit(:name, :description, :price, :category, :user_id)
     end
 end
